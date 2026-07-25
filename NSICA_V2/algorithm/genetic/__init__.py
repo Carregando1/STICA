9005aa8137c740374c68e7742f748147675cb529
Carregando1 <darrenys2015@gmail.com>
2026-07-24 20:07:53 -0400

    Delete NSICA_V2 directory

--- NSICA_V2/algorithm/genetic/__init__.py
+++
@@ -1,5 +0,0 @@
-from .mutation import Mutator, ArbitraryRulesetMutator
-from .optimize import Optimizer
-#There is no CurrentState class anymore. See state.py for more details.
-from .transition import transition
-from .nsica import NSICA,NSICAPerceive,NSICAUpdate
--- NSICA_V2/algorithm/objectives/sampleobj.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def sampleobj(grid):
-    """
-    Calculate the sum of all values in grid. Test objective for n-state SICA generalization.
-    """
-    return -(np.sum(grid))
