68050d7ecb19c38cc8218fb32c39b0279fa9a047
Carregando1 <darrenys2015@gmail.com>
2026-08-13 00:20:56 -0400

    Add files via upload

---
+++ STICA-Main/STICA2S/algorithm/genetic/__init__.py
@@ -0,0 +1,5 @@
+from algorithm.genetic.mutation import Mutator, RulesetMutator, ArbitraryRulesetMutator, MutationSet
+from algorithm.genetic.optimize import Optimizer
+from algorithm.genetic.state import CurrentState
+from algorithm.genetic.transition import transition
+from algorithm.genetic.stica2s import STICA2S,STICA2SPerceive,STICA2SUpdate
