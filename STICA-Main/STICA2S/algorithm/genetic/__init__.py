5198524ff5b720bb097249b417d49f9c52b1c5ff
Carregando1 <darrenys2015@gmail.com>
2026-08-13 00:14:57 -0400

    Delete STICA-Main directory

--- STICA-Main/STICA2S/algorithm/genetic/__init__.py
+++
@@ -1,5 +0,0 @@
-from algorithm.genetic.mutation import Mutator, RulesetMutator, ArbitraryRulesetMutator, MutationSet
-from algorithm.genetic.optimize import Optimizer
-from algorithm.genetic.state import CurrentState
-from algorithm.genetic.transition import transition
-from algorithm.genetic.stica2s import STICA2S,STICA2SPerceive,STICA2SUpdate
--- STICA-Main/STICA2S/arraystat.py
+++
@@ -1,4 +0,0 @@
-#Calculates mean and std of arr.
-import numpy as np
-def stat(arr):
-       return [np.mean(arr), np.std(arr)]
