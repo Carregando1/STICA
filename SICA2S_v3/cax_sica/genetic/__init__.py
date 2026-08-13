fd0c39267399511146844d3d80c1d4b822a356a8
Carregando1 <darrenys2015@gmail.com>
2026-08-13 00:12:41 -0400

    Delete SICA2S_v3 directory

--- SICA2S_v3/cax_sica/genetic/__init__.py
+++
@@ -1,5 +0,0 @@
-from cax_sica.genetic.mutation import Mutator, RulesetMutator, ArbitraryRulesetMutator, MutationSet
-from cax_sica.genetic.optimize import Optimizer
-from cax_sica.genetic.state import CurrentState
-from cax_sica.genetic.transition import transition
-from cax_sica.genetic.sica2s import SICA2S,SICA2SPerceive,SICA2SUpdate
